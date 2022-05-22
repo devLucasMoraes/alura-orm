8a1c460e9267246738bd019d30cf0d391f4981bc
devLucasMoraes <devlucasmoraes@gmai.com>
2022-05-22 13:32:55 -0300

    criado route pessoas

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,8 +1,10 @@
 const bodyParser = require('body-parser')
+const pessoas = require('./pessoasRoute')
 
 module.exports = app => {
 // metodo use serve para avisar para o express que vai ter alguma blibioteca que vai fazer um 
 // meio de campo entre as requisicoes e o express 
     app.use(bodyParser.json())
     app.get('/', (req, res) => res.send('ola!!'))
+    app.use(pessoas)
 }
---
+++ api/routes/pessoasRoute.js
@@ -0,0 +1,10 @@
+const { Router } = require('express')
+const PessoaController = require('../controllers/PessoaController')
+
+const router = Router()
+
+// const pController = new PessoaController nao precisa instanciar isso pois colocamos static 
+router.get('/pessoas', PessoaController.pegaTodasAsPessoas)
+
+module.exports = router
+
