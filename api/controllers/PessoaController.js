966e93df58d6e5a0e75e3121809864a62676c800
devLucasMoraes <devlucasmoraes@gmai.com>
2022-05-25 20:45:45 -0300

    criado escopo de associacao aulasMatriculadas e metodo e rota conrespondente

--- api/controllers/PessoaController.js
+++ api/controllers/PessoaController.js
@@ -91,6 +91,21 @@ class PessoaController {
             return res.status(500).json(error.message)
         }
     }
+    
+    static async pegaTodasAsMatriculasAtivas(req, res) {
+        const {estudanteId} = req.params
+        try {
+            const pessoa = await database.Pessoas.findOne({
+                where: {
+                    id: Number(estudanteId)
+                }
+            })
+            const matriculas = await pessoa.getAulasMatriculadas()
+            return res.status(200).json(matriculas)
+        } catch (error) {
+            return res.status(500).json(error.message)
+        }
+    }
 
     static async pegaUmaMatricula(req, res) {
         const {estudanteId, matriculaId} = req.params
